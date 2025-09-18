89be7902654bda8b75a0fb369792c2d410aa4897
Tuna240425 <ynalee1130@gmail.com>
2025-09-18 17:57:35 +0900

    ReactionCardsSection 오류 해결 시도

--- src/Components/NavigationSection/ReactionCardsSection.tsx
+++ src/Components/NavigationSection/ReactionCardsSection.tsx
@@ -10,7 +10,7 @@ const reactionCards = [
     description: "최고의 지혜를 뽐내는 1명에게 부여하는 나의 '감탄' 입니다. :)",
     subDescription: "1명 밖에 선택할 수 없으니, 신중하게 나의 경의를 표현해보세요!",
     position: { left: "-100px", top: "74.80px" },
-    hoverPosition: { left: "0px", top: "0px" }, // 호버 시 위치
+    hoverPosition: { left: "0px", top: "0px" },
     rotation: "rotate-[-15deg]",
     iconPosition: { left: "16.36px", top: "20.72px" },
     cardSize: { width: "w-[345px]", height: "h-[495px]" },
@@ -25,7 +25,7 @@ const reactionCards = [
     description: "다른 사람들에게도 보여주고 싶은 3명의 지혜를 뽑는 나의 '추천' 입니다.",
     subDescription: "3명을 선택할 수 있으니, 동료 크루의 지혜에 감탄을 보내주세요!",
     position: { left: "408.43px", top: "0" },
-    hoverPosition: { left: "345px", top: "0px" }, // 호버 시 위치
+    hoverPosition: { left: "345px", top: "0px" },
     rotation: "rotate-[15deg]",
     iconPosition: { left: "13.36px", top: "20.52px" },
     cardSize: { width: "w-[345px]", height: "h-[495px]" },
@@ -40,7 +40,7 @@ const reactionCards = [
     description: "나의 지혜와 완전히 같지는 않지만 새로운 관점과 재미를 준 지혜를 뽑는 나의 '존중' 입니다.",
     subDescription: "5명을 골라, 소소한 존중을 표현해보자구요!",
     position: { left: "601.75px", top: "75.26px" },
-    hoverPosition: { left: "690px", top: "0px" }, // 호버 시 위치
+    hoverPosition: { left: "690px", top: "0px" },
     rotation: "rotate-[-15deg]",
     iconPosition: { left: "16.43px", top: "20.70px" },
     cardSize: { width: "w-[345px]", height: "h-[495px]" },
@@ -55,7 +55,7 @@ const reactionCards = [
     description: "조금 더 분발한다면 더 좋은 지혜에 도전해볼 수 있을 지혜를 뽑는 나의 '응원' 입니다.",
     subDescription: "3명을 골라, 힘차게 어깨를 주물러 주자구요!",
     position: { left: "1105.25px", top: "0" },
-    hoverPosition: { left: "1035px", top: "0px" }, // 호버 시 위치
+    hoverPosition: { left: "1035px", top: "0px" },
     rotation: "rotate-[15deg]",
     iconPosition: { left: "13.36px", top: "20.52px" },
     cardSize: { width: "w-[345px]", height: "h-[495px]" },
@@ -66,8 +66,14 @@ const reactionCards = [
 export const ReactionCardsSection = (): JSX.Element => {
   return (
     <section 
-      className="w-full flex justify-center px-4 sm:px-6 lg:px-8"
-      style={{ paddingTop: '135px', paddingBottom: '135px' }}
+      className="w-full px-4 sm:px-6 lg:px-8"
+      style={{ 
+        paddingTop: '135px', 
+        paddingBottom: '135px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
     >
       {/* 데스크톱: 개별 카드 호버 애니메이션이 적용된 absolute 레이아웃 */}
       <div className="hidden xl:block relative w-[1277.57px] h-[464.77px] rounded-[20px]">
